84c0f7274f0dcac99ce025f2e069bbcdf00d1dd4
kiriclope <kiriclop@gmail.com>
2023-12-17 22:25:19 +0100

    Added scimax.el

---
+++ scimax.el
@@ -0,0 +1,8 @@
+;;; scimax.el --- New org-babel functions for src blocks
+
+(require 'scimax-ob)
+(require 'scimax-org-images)
+(require 'scimax-org-src-blocks)
+(require 'scimax-jupyter)
+
+(provide 'my-package)
